d864944debb6801b0925959c5a05b7d38c6e0a99
masporo <34471602+masporo@users.noreply.github.com>
2018-02-12 11:23:36 +0200

    Update counter.php

--- counter.php
+++ counter.php
@@ -1,7 +1,7 @@
-*Created a file named counter.php
+*CREATED A FILE NAMED COUNTER.PHP
 steps to follow when creating counter.php :
 touch counter.php
-add counter.php
+ git add counter.php
 gedit counter.php (Attach counter.php coding)
 <?php
 
@@ -22,7 +22,6 @@ fwrite($datei, $count);
 fclose($datei);
 ?>
 git commit -m "find all files"
-clone the repository
-and use git pish -u origin master
-https://github.com/masporo/visit-counter.git
-http://justintadlock.com/web-design/counter
+clone the respositor,git remote add origin git@github.com:masporo/visit-counter.git
+ git pish -u origin master
+http://justintadlock.com/web-design/counter (the source)
